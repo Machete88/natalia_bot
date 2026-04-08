865eb541157efb5fb819fa7c1e97b3f8c70b7c52
Machete88 <mockggg@gmail.com>
2026-04-08 03:57:08 +0200

    fix: pronunciation.py exportiert auch handle_voice_pronunciation

--- bot/handlers/pronunciation.py
+++ bot/handlers/pronunciation.py
@@ -1,4 +1,5 @@
 """Kompatibilitaets-Re-Export fuer /pronounce Handler."""
 from bot.handlers.pronounce_handler import cmd_pronounce as handle_pronounce
+from bot.handlers.pronounce_handler import handle_voice_pronounce as handle_voice_pronunciation
 
-__all__ = ["handle_pronounce"]
+__all__ = ["handle_pronounce", "handle_voice_pronunciation"]
